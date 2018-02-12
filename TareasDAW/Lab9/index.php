44bce71e3707d43fc20974513bee13f9eeb3e95b
Martin Helmut Dominguez Alvarez <martinhelmutmh@gmail.com>
2018-02-12 16:50:23 -0600

    Lab9 Terminado

--- TareasDAW/Lab9/index.php
+++ TareasDAW/Lab9/index.php
@@ -8,38 +8,112 @@
     <header>
       <h1>Hola, éste es el nuevo lab</h1>
     </header>
+    <br>
     <section class="container">
       <p><strong>Genera</strong>
+
         <?php
-        $array1 = array(30, 23, 34, 323, 67, 12);
-        $i=0;
-        $suma=0;
-        while ($i <6) {
-            echo $array1{$i}." ";
-            $suma=$suma+$array1{$i};
-            $i++;
-        };
+          $array1 = array(30, 23, 34, 323, 67, 12);
+          $i=0;
+          $suma=0;
+          while ($i <6) {
+              echo $array1{$i}." ";
+              $i++;
+          };
+
+          function promedio($arreglo)
+          {
+              while ($i <6) {
+                  $suma=$suma+$arreglo{$i};
+                  $i++;
+              };
+              echo $suma/6;
+          }
         ?>
+
       </p>
       <p><strong>El promedio es:</strong>
+
         <?php
-        echo $suma/6;
+          promedio($array1);
         ?>
+
       </p>
       <p>
         <strong>La mediana de: </strong>
+
         <?php
-        sort($array1);
-        //Automáticamente ordena
-        $j =0;
-        while ($j <6) {
-            echo $array1{$j}." ";
-            $j++;
-        };
-        echo "<strong>es: </strong>";
-        echo($array1{2}+$array1{3})/2;
+          sort($array1);
+          //Automáticamente ordena
+          function mediana($array)
+          {
+              $j =0;
+              while ($j <6) {
+                  echo $array{$j}." ";
+                  $j++;
+              };
+              echo "<strong>es: </strong>";
+              echo($array{2}+$array{3})/2;
+          }
+          mediana($array1);
         ?>
+
+
+
       </p>
+      <p>
+
+        <?php
+          $array2 = array(56, 105, 12, 80, 22, 36);
+         ?>
+
+        <strong>El arreglo es:
+          <?php
+          $i=0;
+          $suma=0;
+          while ($i <6) {
+              echo $array2{$i}." ";
+              $i++;
+          }; ?>
+        </strong>
+        <ul>
+
+          <?php
+            sort($array2);
+              echo "<li>El arreglo ordenado es:";
+              foreach ($array2 as $key2) {
+                  echo $key2." ";
+              }echo "</li>";
+            rsort($array2);
+              echo "<li>El arreglo ordenado en orden descendiente es:";
+              foreach ($array2 as $key2) {
+                  echo $key2." ";
+              }echo "</li>";
+              echo "<li> La mediana de";
+              mediana($array1);
+              echo "</li>";
+
+              echo "<li> La media es:";
+              promedio($array2);
+              echo"</li>";
+          ?>
+
+        </ul>
+      </p>
+
+      <p>
+        <?php
+          $num=5;
+          $i=0; $j=0;
+          echo "<table>";
+          echo "<thead><td>Número</td><td>Número Cuadrado</td><td>Número Cúbico</td></thead>";
+          for ($i=0; $i < $num; $i++) {
+              echo "<tr> <td>".$i."</td><td>".$i*$i."</td><td>".$i*$i*$i."</td></tr>";
+          }
+          echo"</table>";
+         ?>
+      </p>
+
       <br>
       <h3>Cuestionario</h3>
         <ul>
@@ -66,16 +140,18 @@
 
       <blockquote>
         <p><em>
-          <?php
-          $palabras = array("como ", "serrucho ", "papalote ", "cielo ",
-          "decirte ", "quiero ", "tanto ", "nadador ", "millonario ");
-          shuffle($palabras);
-          foreach ($palabras as $palabra) {
-              echo $palabra;
-          };
 
-          unset($palabra);
+          <?php
+          //Mezclar las palabras
+            $palabras = array("como ", "serrucho ", "papalote ", "cielo ",
+            "decirte ", "quiero ", "tanto ", "nadador ", "millonario ");
+            shuffle($palabras);
+            foreach ($palabras as $palabra) {
+                echo $palabra;
+            };
+            unset($palabra);
           ?>
+
         </em></p>
       </blockquote>
 
