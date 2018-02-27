39a0ab14966439e30cf096c59eb791269016c3ef
Martin Helmut Dominguez Alvarez <martinhelmutmh@gmail.com>
2018-02-26 23:52:44 -0600

    Agregar avance lab13

---
+++ TareasDAW/Lab13/index.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+include("_header.html");
+if (isset($_SESSION['usuario'])) {
+  include("_cuestionario.html");
+}else {
+  //Si no se ha iniciado la sesión se conduce a la página para hacerlo
+  header("location:page.php");
+}
+include("_footer.html");
+?>
---
+++ TareasDAW/Lab13/page.php
@@ -0,0 +1,30 @@
+<?php
+  session_start();
+  include("_header.html");
+  // echo "Bienvenido ".$_POST["usuario"];
+  include("_despliega.html");
+  // Ya está blindado chicos
+
+  if((isset($_POST['user'])) && (isset($_POST['password']))){
+    $nombre=htmlspecialchars($_POST['user']);
+    $password=htmlspecialchars($_POST['password']);
+  }
+
+  if ($nombre=='robleManzano' && $password=='paralelepipedo' && $_POST['age']=='14-17'){
+      echo "Yujuu!! :D";
+      echo $_POST['age'];
+      //Declaremos la existencia de una sesiones
+      $_SESSION['usuario']=$_POST['user'];
+
+      include("_cuestionario.html");
+  }else {
+    echo "
+    <section class=container>
+      Por Favor Ingresa Datos Válidos
+    </section>
+    ";
+  }
+  include("_footer.html");
+
+
+?>
