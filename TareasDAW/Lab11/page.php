1206eb230c78cf3f1667a6c2f4082105bf148338
Martin Helmut Dominguez Alvarez <martinhelmutmh@gmail.com>
2018-02-26 20:00:47 -0600

    Blindar lab

--- TareasDAW/Lab11/page.php
+++ TareasDAW/Lab11/page.php
@@ -3,9 +3,21 @@
   include("_header.html");
   // echo "Bienvenido ".$_POST["usuario"];
   include("_despliega.html");
-  if (isset($_POST['nameField'])){
-    echo "Yujuu!! :D";
-    include("_cuestionario.html");
+  // Ya está blindado chicos
+
+  if((isset($_POST['nameField'])) && (isset($_POST['commentField']))){
+    $nombre=htmlspecialchars($_POST['nameField']);
+    $coment=htmlspecialchars($_POST['commentField']);
+  }else {
+    $nombre;;
+    $coment;
+  }
+
+  if ($nombre && $coment){
+      echo "Yujuu!! :D";
+      include("_cuestionario.html");
+  }else {
+    echo "Por Favor Ingresa Datos Válidos";
   }
   include("_footer.html");
 
